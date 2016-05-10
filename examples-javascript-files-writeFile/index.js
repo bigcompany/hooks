152afec1c9fdbda787b7a0fc6431320012e56274
Marak <marak.squires@gmail.com>
2016-05-10 01:08:30 -0400

    [dist] Remove incorrect cased files

--- examples-javascript-files-writeFile/index.js
+++
@@ -1,8 +0,0 @@
-module['exports'] = function writeFile (hook) {
-  hook.fs.writeFile('testing-file.txt', 'hello', function (err, file) {
-    if (err){ 
-      return hook.res.end(err.message);
-    }
-    hook.res.json(file);
-  });
-};
